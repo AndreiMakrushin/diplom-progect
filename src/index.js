73b16a5ec7a138f718e921d6a6ab46e30daa01ed
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-09-14 22:20:19 +0500

    сделал аккордеон

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import modal from "./modules/modal";
 import anchorLink from "./modules/anchorLink"
 import slider from "./modules/slider"
 import smooth from "./modules/smooth"
+import accordeon from "./modules/accordion"
 modal();
 anchorLink();
 slider();
 smooth();
+accordeon();
--- src/modules/smooth.js
+++ src/modules/smooth.js
@@ -1,17 +1,10 @@
 const smooth = () =>{
     const up = document.querySelector('.up')
-    
     document.addEventListener('scroll', ()=>{
         if (window.pageYOffset > 650) {
             up.style.display = 'block'
         }else{
             up.style.display = 'none'
-        }
-    })
-
-    up.addEventListener('click', (e)=>{
-        if (e.target) {
-            
         }
     })
 }
